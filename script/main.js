5ae00dceeb0ff2c7385d5e352dd4037bccad1d84
Willi Berger <willi.berger@gmx.at>
2020-01-07 17:53:42 +0100

    added different food types: regular, move, disappear

--- script/main.js
+++ script/main.js
@@ -1,18 +1,29 @@
-
+/**
+ * snake game
+ */
+// TODO  'use strict'
 const Direction = {left: 0, up: 1, right: 2, down: 3};
 
-const snakeGame = function (canvas) {
-    var ctx = canvas.getContext('2d')
+const FoodType = {
+    regular: {color: 'red', score: 1},      // regular
+    moving: {color: 'yellow', score: 2},    // moves
+    disappear: {color: 'green', score:2}    // disappears, if eaten produces 3 red
+};
 
+function snakeGame (canvas) {
+    const ctx = canvas.getContext('2d');
     const nRows = 15;
     const nCols = 20;
     const width = ctx.canvas.width;
     const height = ctx.canvas.height;
     const cellWidth = width/nCols;
     const cellHeight = height/nRows;
+    const timeStepMsDefault = 400;  // ATT inititially hard coded in index.html
+    const MoveFoodInterval = 4000;
+    const DisappearFoodInterval =8000;
+    
     var score = 0;
-
-    var  timeStepMsDefault = 500;
+    var foods = [];
     var timeStepMs = 500;
 
     function Snake(x, y) {
@@ -60,9 +71,10 @@ const snakeGame = function (canvas) {
             this.tail[0] = {x: this.x, y: this.y};
         }
 
-        this.eat = function() {
+        this.eat = function(food) {
+            // ToDo grow depending on food type
             this.tail.push({x: this.x, y: this.y});
-            updateScore(++score);
+            updateScore(score += food.type.score);
             console.debug( `eat: ${this.tail.length}:`, this.tail);
         }
 
@@ -85,47 +97,88 @@ const snakeGame = function (canvas) {
         }
     }
 
-    function Food(x, y) {
+    function Food(x, y, type) {
         this.x = x;
         this.y = y;
-        console.log("new food:", this)
+        this.type = type;
+        console.log("new food:", this);
+
+        var timer;
+        this.cleanUp = function () {
+            if (timer) {
+                clearTimeout(timer);
+            }
+        }
+
+        this.move = function(self) {
+            let newPlace = this.randomFreeCell();
+            console.debug("move food to new place ", newPlace);
+            self.x = newPlace.x;
+            self.y = newPlace.y;
+            timer = setTimeout(self.move, MoveFoodInterval, self);
+        }
 
         this.stroke = function () {
             //console.debug(`food.stroke x: ${this.x}, y: ${this.y}`);
             ctx.save();
             ctx.translate(this.x * cellWidth, this.y * cellHeight);
             ctx.beginPath();
-            ctx.fillStyle = 'red';
+            ctx.fillStyle = type.color;
             ctx.arc(cellWidth / 2, cellHeight / 2, Math.min(cellWidth, cellHeight) / 2, 0, 2*Math.PI);
             ctx.fill();
             ctx.restore();
         }
     }
 
-    this.placeFood = function(occupied) {
-        // Food must not be on/under snake
-        do {
+    this.randomFreeCell = function () {
+        do {  // ToDo in theory this could be an enless loop 
             var cell ={x: Math.floor(Math.random() * nCols), y: Math.floor(Math.random() * nRows)};
-        } while (occupied.some(elem => elem.x === cell.x && elem.y === cell.y))
+        } while (
+            snake.tail.some(elem => elem.x === cell.x && elem.y === cell.y) ||
+            foods.some(f => f.x === cell.x && f.y === cell.y))
+        
+        return cell;
+    }
 
-        return new Food(cell.x, cell.y);
+    this.placeFood = function() {
+        // Food must not be on/under snake
+        let cell = this.randomFreeCell();
+        let dice = Math.floor(Math.random() * 10);
+        type = dice <= 5 ? FoodType.regular : dice <= 7 ? FoodType.moving : FoodType.disappear;
+
+        let food = new Food(cell.x, cell.y, type);       
+        if (food.type === FoodType.moving) {
+            timer = setTimeout(food.move, MoveFoodInterval, food);
+        } else if (food.type === FoodType.disappear) {  // ToDo cleanup timer if food is eaten before
+            setTimeout(this.removeFood, DisappearFoodInterval, this, food);
+        }
+        return food;
+    }
+
+    this.removeFood = function (self, food) {
+        console.debug("remove food ", food)
+        foods = foods.filter(f => f.x != food.x && f.y != food.y);
     }
 
     function updateAll() {
         ctx.clearRect(0, 0, width, height);
 
         if (snake.canWalk()) {
-            if (snake.x === food.x && snake.y === food.y) {
-                snake.eat();
-                food = this.placeFood(snake.tail);
-                food.stroke();
+            let foundFood = foods.find(f => f.x === snake.x && f.y === snake.y);
+            if (foundFood) {
+                snake.eat(foundFood);
+                foundFood.cleanUp();
+                foods = foods.filter(f => f.x != foundFood.x && f.y != foundFood.y);
             }
             snake.walk()
-            setTimeout(updateAll, timeStepMs);
+            let timer = setTimeout(updateAll, timeStepMs);
         } else {
             console.log('game over');
         }
-        food.stroke();
+        if (foods.length == 0) {
+            foods.push(this.placeFood());
+        }
+        foods.forEach(f => f.stroke());
         snake.stroke();
     }
 
@@ -203,8 +256,8 @@ const snakeGame = function (canvas) {
     var snake = new Snake(Math.floor(nCols/2), Math.floor(nRows/2));
     snake.dir = Direction.right;
 
-    // Init food
-    var food = this.placeFood(snake.tail);
+    // Init first food
+    foods.push(this.placeFood());
 
     // Do the first update
     updateAll();
@@ -215,11 +268,14 @@ function updateScore(score) {
     document.getElementById("score").innerHTML = score;
 }
 
+function startGame() {
+    var canvas = document.getElementById("canvas");
+    snakeGame(canvas);
+}
 
 document.onreadystatechange = function () {
     if (document.readyState === 'complete') {
         console.debug('document ready')
-        var canvas = document.getElementById("canvas")
-        snakeGame(canvas)
+        startGame()
     }
 }
